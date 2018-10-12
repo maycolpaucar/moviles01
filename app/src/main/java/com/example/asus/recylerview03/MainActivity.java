1e4ebb6cc28aca224ae98955d47e0727749ace16
PC-19 <mpaucars256@gmail.com>
2018-10-12 09:25:26 -0500

    Modificando archivos

--- app/src/main/java/com/example/asus/recylerview03/MainActivity.java
+++ app/src/main/java/com/example/asus/recylerview03/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //mostrando un mensaje
+        Toast.makeText( this, "Agregando los elementos",Toast.LENGTH_SHORT).show();
 
         ArrayList<Fuente> lista = new ArrayList<Fuente>();
         lista.add(new Fuente("Imagen Numero 1",R.drawable.uno,0));
